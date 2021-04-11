cda644796343e86a7970efc8facc57e106adeb36
Stackbit Code Editor <projects@stackbit.com>
2021-04-11 17:42:42 +0000

    CallButton.js: changed

--- src/components/CallButton.js
+++ src/components/CallButton.js
@@ -4,7 +4,8 @@ export default class CallButton extends React.Component{
     render(){
         return{
             <script id="setmore_script" type="text/javascript" src="https://my.setmore.com/webapp/js/src/others/setmore_iframe.js"></script>
-            <a id="Setmore_button_iframe" style="float:none" href="https://my.setmore.com/bookingpage/2433fc45-ddd4-4e7d-bce3-6346cbad6db8"><img border="none" src="https://storage.googleapis.com/setmore-assets/2.0/Images/Integration/book-now-blue.svg" alt="Book an appointment with CreativePixel using Setmore" /></a>
+            <a id="Setmore_button_iframe" style="float:none" href="https://my.setmore.com/bookingpage/2433fc45-ddd4-4e7d-bce3-6346cbad6db8"><img border="none" src="https://storage.googleapis.com/setmore-assets/2.0/Images/Integration/book-now-blue.svg" alt="Book an appointment with CreativePixel using Setmore" />
+            </a>
         }
     }
 
