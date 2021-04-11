988fc1895882e44330b76cd45a694c600b1930ae
Stackbit Code Editor <projects@stackbit.com>
2021-04-11 17:49:08 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import _ from 'lodash';
+import CallButton from './CallButton';
 
 import {Link, withPrefix, classNames} from '../utils';
 import Icon from './Icon';
-import CallButton from './CallButton';
 
 export default class Action extends React.Component {
     render() {
@@ -12,7 +12,15 @@ export default class Action extends React.Component {
         let action_icon = _.get(action, 'icon', null) || 'arrow-left';
         let action_icon_pos = _.get(action, 'icon_position', null) || 'left';
         return (
-            <CallButton />
+            <Link href={withPrefix(_.get(action, 'url', null))}
+              {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
+              {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}
+              className={classNames({'button': (action_style === 'primary') || (action_style === 'secondary'), 'secondary': action_style === 'secondary', 'has-icon': _.get(action, 'has_icon', null)})}>
+              {_.get(action, 'has_icon', null) && (
+                <Icon {...this.props} icon={action_icon} />
+              )}
+              <span className={classNames({'order-first': action_icon_pos === 'right'})}>{_.get(action, 'label', null)}</span>
+            </Link>
         );
     }
 }
